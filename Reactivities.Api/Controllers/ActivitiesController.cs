5484fa9a83eba7e95230a4dfa694edd1593dca32
Debayan Mitra <dmitra@csod.com>
2021-04-08 14:29:46 +0530

    End of section 10

--- Reactivities.Api/Controllers/ActivitiesController.cs
+++ Reactivities.Api/Controllers/ActivitiesController.cs
@@ -12,19 +12,19 @@ namespace Reactivities.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query{ Id = id });
+            return HandleResult(await Mediator.Send(new Details.Query{ Id = id }));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command{
+            return HandleResult(await Mediator.Send(new Create.Command{
                Activity = activity 
             }));
         }
@@ -33,7 +33,7 @@ namespace Reactivities.Api.Controllers
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
             activity.Id = id;
-            return Ok(await Mediator.Send(new Edit.Command{
+            return HandleResult(await Mediator.Send(new Edit.Command{
                Activity = activity 
             }));
         }
@@ -41,7 +41,7 @@ namespace Reactivities.Api.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
-            return Ok(await Mediator.Send(new Delete.Command{
+            return HandleResult(await Mediator.Send(new Delete.Command{
                Id = id 
             }));
         }
--- Reactivities.Application/Activities/Delete.cs
+++ Reactivities.Application/Activities/Delete.cs
@@ -2,18 +2,19 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Reactivities.Application.Core;
 using Reactivities.Persistence;
 
 namespace Reactivities.Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -21,12 +22,19 @@ namespace Reactivities.Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = _context.Activities.FindAsync(request.Id);
+
+                if(activity == null)
+                    return null;
+
                 _context.Activities.Remove(activity.Result);
-                await _context.SaveChangesAsync();
-                return Unit.Value;
+                var result = await _context.SaveChangesAsync() > 0;
+                if(!result)
+                    return Result<Unit>.Failure("Failed to delete the activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
